c93656e80d56ceb05464ec3cc2a8f5390fd58fbf
Dylan Parker <dylanparkerr@gmail.com>
2022-12-13 23:24:58 -0600

    intial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dylanparkerr/go-project
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,8 @@
+// https://hackersandslackers.com/create-your-first-golang-app/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world.")
+}
